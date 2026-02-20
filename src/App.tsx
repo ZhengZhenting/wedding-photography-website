6230ab4ef00aebf9c378b9c5e7ea8dc22c14a581
ZhengZhenting <zhzh9685@bht-berlin.de>
2026-02-20 12:54:42 +0100

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/wedding-photography-website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/portfolio" element={<Portfolio />} />
