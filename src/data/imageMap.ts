55af1ddbdf6908b25f53985f4915b2a7419ef379
ZhengZhenting <zhzh9685@bht-berlin.de>
2026-02-21 12:26:45 +0100

    new file:   src/assets/images/cover-1.jpg 	new file:   src/assets/images/detail-1a.jpg 	new file:   src/assets/images/detail-1b.jpg 	new file:   src/assets/images/detail-1c.jpg 	new file:   src/assets/images/detail-1d.jpg 	new file:   src/assets/images/detail-1e.jpg 	new file:   src/assets/images/detail-1f.jpg 	new file:   src/assets/images/detail-1g.jpg 	new file:   src/assets/images/detail-1h.jpg 	new file:   src/assets/images/detail-1i.jpg 	deleted:    src/assets/images/wedding-cover-1.jpg 	deleted:    src/assets/images/wedding-cover-2.jpg 	deleted:    src/assets/images/wedding-cover-3.jpg 	deleted:    src/assets/images/wedding-cover-4.jpg 	deleted:    src/assets/images/wedding-detail-1.jpg 	deleted:    src/assets/images/wedding-detail-2.jpg 	deleted:    src/assets/images/wedding-detail-3.jpg 	deleted:    src/assets/images/wedding-detail-4.jpg 	modified:   src/data/imageMap.ts 	modified:   src/data/weddings.ts

--- src/data/imageMap.ts
+++ src/data/imageMap.ts
@@ -1,24 +1,182 @@
 // Image map for dynamic imports
 import heroImg from "@/assets/images/hero.jpg";
-import cover1 from "@/assets/images/wedding-cover-1.jpg";
-import cover2 from "@/assets/images/wedding-cover-2.jpg";
-import cover3 from "@/assets/images/wedding-cover-3.jpg";
-import cover4 from "@/assets/images/wedding-cover-4.jpg";
-import detail1 from "@/assets/images/wedding-detail-1.jpg";
-import detail2 from "@/assets/images/wedding-detail-2.jpg";
-import detail3 from "@/assets/images/wedding-detail-3.jpg";
-import detail4 from "@/assets/images/wedding-detail-4.jpg";
 import aboutPortrait from "@/assets/images/about-portrait.jpg";
+
+import cover1 from "@/assets/images/cover-1.jpg";
+import cover2 from "@/assets/images/cover-2.jpg";
+import cover3 from "@/assets/images/cover-3.jpg";
+import cover4 from "@/assets/images/cover-4.jpg";
+import cover5 from "@/assets/images/cover-5.jpg";
+import cover6 from "@/assets/images/cover-6.jpg";
+import cover7 from "@/assets/images/cover-7.jpg";
+import cover8 from "@/assets/images/cover-8.jpg";
+
+import detail1a from "@/assets/images/detail-1a.jpg";
+import detail1b from "@/assets/images/detail-1b.jpg";
+import detail1c from "@/assets/images/detail-1c.jpg";
+import detail1d from "@/assets/images/detail-1d.jpg";
+import detail1e from "@/assets/images/detail-1e.jpg";
+import detail1f from "@/assets/images/detail-1f.jpg";
+import detail1g from "@/assets/images/detail-1g.jpg";
+import detail1h from "@/assets/images/detail-1h.jpg";
+import detail1i from "@/assets/images/detail-1i.jpg";
+
+import detail2a from "@/assets/images/detail-2a.jpg";
+import detail2c from "@/assets/images/detail-2c.jpg";
+import detail2d from "@/assets/images/detail-2d.jpg";
+import detail2f from "@/assets/images/detail-2f.jpg";
+import detail2g from "@/assets/images/detail-2g.jpg";
+import detail2h from "@/assets/images/detail-2h.jpg";
+import detail2i from "@/assets/images/detail-2i.jpg";
+import detail2j from "@/assets/images/detail-2j.jpg";
+import detail2k from "@/assets/images/detail-2k.jpg";
+import detail2l from "@/assets/images/detail-2l.jpg";
+
+import detail3a from "@/assets/images/detail-3a.jpg";
+import detail3b from "@/assets/images/detail-3b.jpg";
+import detail3c from "@/assets/images/detail-3c.jpg";
+import detail3d from "@/assets/images/detail-3d.jpg";
+import detail3e from "@/assets/images/detail-3e.jpg";
+import detail3f from "@/assets/images/detail-3f.jpg";
+import detail3g from "@/assets/images/detail-3g.jpg";
+import detail3h from "@/assets/images/detail-3h.jpg";
+
+import detail4a from "@/assets/images/detail-4a.jpg";
+import detail4b from "@/assets/images/detail-4b.jpg";
+import detail4c from "@/assets/images/detail-4c.jpg";
+import detail4d from "@/assets/images/detail-4d.jpg";
+import detail4e from "@/assets/images/detail-4e.jpg";
+import detail4f from "@/assets/images/detail-4f.jpg";
+import detail4g from "@/assets/images/detail-4g.jpg";
+import detail4h from "@/assets/images/detail-4h.jpg";
+import detail4i from "@/assets/images/detail-4i.jpg";
+
+import detail5a from "@/assets/images/detail-5a.jpg";
+import detail5b from "@/assets/images/detail-5b.jpg";
+import detail5c from "@/assets/images/detail-5c.jpg";
+import detail5d from "@/assets/images/detail-5d.jpg";
+import detail5e from "@/assets/images/detail-5e.jpg";
+import detail5f from "@/assets/images/detail-5f.jpg";
+import detail5g from "@/assets/images/detail-5g.jpg";
+import detail5h from "@/assets/images/detail-5h.jpg";
+import detail5i from "@/assets/images/detail-5i.jpg";
+import detail5j from "@/assets/images/detail-5j.jpg"; 
+import detail5k from "@/assets/images/detail-5k.jpg";
+import detail5l from "@/assets/images/detail-5l.jpg";
+
+import detail6a from "@/assets/images/detail-6a.jpg";
+import detail6b from "@/assets/images/detail-6b.jpg";
+import detail6c from "@/assets/images/detail-6c.jpg";
+import detail6d from "@/assets/images/detail-6d.jpg";
+import detail6e from "@/assets/images/detail-6e.jpg";
+import detail6f from "@/assets/images/detail-6f.jpg";
+
+import detail7a from "@/assets/images/detail-7a.jpg";
+import detail7b from "@/assets/images/detail-7b.jpg";
+import detail7c from "@/assets/images/detail-7c.jpg";
+import detail7d from "@/assets/images/detail-7d.jpg";
+import detail7e from "@/assets/images/detail-7e.jpg";
+import detail7f from "@/assets/images/detail-7f.jpg";
+import detail7g from "@/assets/images/detail-7g.jpg";
+
+import detail8a from "@/assets/images/detail-8a.jpg"; 
+import detail8b from "@/assets/images/detail-8b.jpg";
+import detail8c from "@/assets/images/detail-8c.jpg";
+import detail8d from "@/assets/images/detail-8d.jpg";
+import detail8e from "@/assets/images/detail-8e.jpg";
+import detail8f from "@/assets/images/detail-8f.jpg";
+import detail8g from "@/assets/images/detail-8g.jpg";
+import detail8h from "@/assets/images/detail-8h.jpg";
+
 
 export const imageMap: Record<string, string> = {
   hero: heroImg,
-  "wedding-cover-1": cover1,
-  "wedding-cover-2": cover2,
-  "wedding-cover-3": cover3,
-  "wedding-cover-4": cover4,
-  "wedding-detail-1": detail1,
-  "wedding-detail-2": detail2,
-  "wedding-detail-3": detail3,
-  "wedding-detail-4": detail4,
-  "about-portrait": aboutPortrait,
-};
+  aboutPortrait: aboutPortrait,
+
+  "cover-1": cover1,
+  "cover-2": cover2,
+  "cover-3": cover3,
+  "cover-4": cover4,
+  "cover-5": cover5,
+  "cover-6": cover6,
+  "cover-7": cover7,
+  "cover-8": cover8,
+
+  "detail-1a": detail1a,
+  "detail-1b": detail1b,
+  "detail-1c": detail1c,
+  "detail-1d": detail1d,
+  "detail-1e": detail1e,
+  "detail-1f": detail1f,
+  "detail-1g": detail1g,
+  "detail-1h": detail1h,
+  "detail-1i": detail1i,
+
+  "detail-2a": detail2a,
+  "detail-2c": detail2c,
+  "detail-2d": detail2d,
+  "detail-2f": detail2f,
+  "detail-2g": detail2g,
+  "detail-2h": detail2h,
+  "detail-2i": detail2i,
+  "detail-2j": detail2j,
+  "detail-2k": detail2k,
+  "detail-2l": detail2l,
+
+  "detail-3a": detail3a,
+  "detail-3b": detail3b,
+  "detail-3c": detail3c,
+  "detail-3d": detail3d,
+  "detail-3e": detail3e,
+  "detail-3f": detail3f,
+  "detail-3g": detail3g,
+  "detail-3h": detail3h,
+
+  "detail-4a": detail4a,
+  "detail-4b": detail4b,
+  "detail-4c": detail4c,
+  "detail-4d": detail4d,
+  "detail-4e": detail4e,
+  "detail-4f": detail4f,
+  "detail-4g": detail4g,
+  "detail-4h": detail4h,
+  "detail-4i": detail4i,
+
+  "detail-5a": detail5a,
+  "detail-5b": detail5b,
+  "detail-5c": detail5c,
+  "detail-5d": detail5d,
+  "detail-5e": detail5e,
+  "detail-5f": detail5f,
+  "detail-5g": detail5g,
+  "detail-5h": detail5h,
+  "detail-5i": detail5i,
+  "detail-5j": detail5j,
+  "detail-5k": detail5k,
+  "detail-5l": detail5l,
+
+  "detail-6a": detail6a,
+  "detail-6b": detail6b,
+  "detail-6c": detail6c,
+  "detail-6d": detail6d,
+  "detail-6e": detail6e,
+  "detail-6f": detail6f,
+
+  "detail-7a": detail7a,
+  "detail-7b": detail7b,
+  "detail-7c": detail7c,
+  "detail-7d": detail7d,
+  "detail-7e": detail7e,
+  "detail-7f": detail7f,
+  "detail-7g": detail7g,
+
+  "detail-8a": detail8a,
+  "detail-8b": detail8b,
+  "detail-8c": detail8c,
+  "detail-8d": detail8d,
+  "detail-8e": detail8e,
+  "detail-8f": detail8f,
+  "detail-8g": detail8g,
+  "detail-8h": detail8h,
+
+}
